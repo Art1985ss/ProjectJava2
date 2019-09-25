dcc95099dad8d3d84093d247a1e0c2f523315ee0
Art1985ss <art_1985@inbox.lv>
2019-09-25 21:35:12 +0300

    #0 Added initial project files

---
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -0,0 +1,46 @@
+package com.javaguru.shoppinglist;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class ShoppingListApplication {
+
+    public static void main(String[] args) {
+        Map<Long, Product> productRepository = new HashMap<>();
+        Long productIdSequence = 0L;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                System.out.println("1. Create product");
+                System.out.println("2. Find product by id");
+                System.out.println("3. Exit");
+                Integer userInput = Integer.valueOf(scanner.nextLine());
+                switch (userInput) {
+                    case 1:
+                        System.out.println("Enter product name: ");
+                        String name = scanner.nextLine();
+                        System.out.println("Enter product price: ");
+                        BigDecimal price = new BigDecimal(scanner.nextLine());
+                        Product product = new Product();
+                        product.setName(name);
+                        product.setPrice(price);
+                        product.setId(productIdSequence);
+                        productRepository.put(productIdSequence, product);
+                        productIdSequence++;
+                        System.out.println("Result: " + product.getId());
+                    case 2:
+                        System.out.println("Enter product id: ");
+                        long id = scanner.nextLong();
+                        Product findProductResult = productRepository.get(id);
+                        System.out.println(findProductResult);
+                    case 3:
+                        return;
+                }
+            } catch (Exception e) {
+                System.out.println("Error! Please try again.");
+            }
+        }
+    }
+}
